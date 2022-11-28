5b61b65afb897acc8d80e23a0f83ed296730752b
SheatNoisette <sheatnoisette@protonmail.com>
2022-11-28 16:49:33 +0100

    example: Add simple CLI parsing

---
+++ examples/from_stdin.v
@@ -0,0 +1,20 @@
+module main
+
+import vopher
+import os
+
+fn main () {
+	if os.args.len != 1 {
+		println('No arguments needed')
+		return
+	}
+
+	// Get from stdin
+	line := os.get_line()
+	parse := vopher.parse_line(line) or {
+		println('Error parsing line: $err')
+		exit(1)
+	}
+
+	println('Parsed line: $parse')
+}
